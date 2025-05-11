5d17165c0cfdc7af552f59cf424f51f14dd8bbb0
JSusman <jonsusman@gmail.com>
2025-05-11 09:32:17 -0700

    Matrix

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-der = 0
-fer = 1
-plur = "grr"
+import numpy as np
+
+grid = np.zeros((6,7))
+
+for i in range(0,len(grid)):
+  grid[i] = i
+  for j in range(0,7):
+    for k in range(0,6):
+     n = j + (7*i)
+      
+    grid[i,j] = n
+    
+
+print(grid)
